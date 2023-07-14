67dd90e6026d5d914398dad78a97cec3f2006fbb
aminubilhuda <aminubilhuda0707@gmail.com>
2023-07-14 12:44:00 +0700

    menambahkan maps

--- app/src/main/java/com/aminu/sewagudangapp/ui/SecondFragment.kt
+++ app/src/main/java/com/aminu/sewagudangapp/ui/SecondFragment.kt
@@ -1,12 +1,18 @@
 package com.aminu.sewagudangapp.ui
 
 import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.PackageManagerCompat
+import androidx.core.location.LocationManagerCompat.getCurrentLocation
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
@@ -14,11 +20,21 @@ import com.aminu.sewagudangapp.R
 import com.aminu.sewagudangapp.application.WarehouseApp
 import com.aminu.sewagudangapp.databinding.FragmentSecondBinding
 import com.aminu.sewagudangapp.model.Warehouse
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class SecondFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerDragListener {
 
     private var _binding: FragmentSecondBinding? = null
     private val binding get() = _binding!!
@@ -28,6 +44,9 @@ class SecondFragment : Fragment() {
     }
     private val args : SecondFragmentArgs by navArgs()
     private var warehouse: Warehouse? = null
+    private lateinit var mMap: GoogleMap
+    private var currentLatLang: LatLng? = null
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -58,6 +77,13 @@ class SecondFragment : Fragment() {
             binding.addressEditText.setText(warehouse?.address)
             binding.ownerEditText.setText(warehouse?.owner)
         }
+
+        // binding google map
+        val mapFragment = childFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+        checkPermission()
+
         val name = binding.nameEditText.text
         val address = binding.addressEditText.text
         val owner = binding.ownerEditText.text
@@ -92,4 +118,67 @@ class SecondFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        // implement drag marker
+
+        val uiSettings = mMap.uiSettings
+        uiSettings.isZoomControlsEnabled = true
+        val sydney = LatLng(-34.0, 151.0)
+        val markerOption = MarkerOptions()
+            .position(sydney)
+            .title("Test")
+            .draggable(true)
+            .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_warehouse_pin))
+        mMap.addMarker(markerOption)
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15f))
+        mMap.setOnMarkerDragListener(this)
+    }
+
+    override fun onMarkerDrag(p0: Marker) {}
+
+    override fun onMarkerDragEnd(marker: Marker) {
+        val newPosition = marker.position
+        currentLatLang = LatLng(newPosition.latitude, newPosition.longitude)
+        Toast.makeText(context, currentLatLang.toString(), Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onMarkerDragStart(p0: Marker) {
+    }
+
+    private fun checkPermission() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(applicationContext)
+        if (ContextCompat.checkSelfPermission(
+                applicationContext,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            getCurrentLocation()
+        } else {
+            Toast.makeText(applicationContext, "Akes lokasi ditolak", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun getCurrentLocation() {
+        // ngecek jika permission tidak disetujui maka akan berhenti di kondisi if
+        if(ActivityCompat.checkSelfPermission(
+                applicationContext,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location ->
+                if (location != null) {
+                    var latLang = LatLng(location.latitude, location.longitude)
+                    currentLatLang = latLang
+                    var title = "Marker"
+
+                }
+            }
+    }
 }
+
